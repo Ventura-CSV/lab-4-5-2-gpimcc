a5410d48e1b291be467fdb915d478af9ecae3f4c
Gabriella <gabriella_pimentel1@my.vcccd.edu>
2024-06-27 13:57:54 -0400

    completed lab

--- main.py
+++ main.py
@@ -4,6 +4,11 @@ import random
 def main():
     total = 0
     numbers = []
+    while total <= 100:
+        num = random.randint(1, 100)
+        numbers.append(num)
+        total += num
+    total = total - numbers[-1]
     """
     ########################################
     Code Your Program here
